feea2270c1637eadb68660db60f7f9ba22336bd2
Tomer Negad <tomernegad@gmail.com>
2024-12-11 17:50:29 +0200

    added gif for which one it winning

--- src/components/StateResults.jsx
+++ src/components/StateResults.jsx
@@ -28,11 +28,23 @@ export default function StateResults() {
     return <div>Loading...</div>;
   }
 
+  const trumpGif =
+    "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3RxZjR0OWp0aWo3cGxiZHowdmt5YWN3ZWtoNmh6cThrOXl3bGVuNSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/qOLT5C2SxNINSREyx9/200.webp";
+  const harrisGif =
+    "https://media3.giphy.com/media/Hi0scxlr9BM7znd3Wy/200.webp?cid=ecf05e472ln3ry7mks3dthw4ukq8jvb94yqlhvt4y5ghenjp&ep=v1_gifs_search&rid=200.webp&ct=g";
+
   return (
     <div className="results-container">
       <h1>Election Results for {stateName}</h1>
       <p>Trump Votes: {results.Trump}%</p>
       <p>Harris Votes: {results.Harris}%</p>
+      <div className="gif-container">
+        <img
+          src={results.Trump > results.Harris ? trumpGif : harrisGif}
+          alt={results.Trump > results.Harris ? "Trump GIF" : "Harris GIF"}
+          className="results-gif"
+        />
+      </div>
       <Link to="/Map" className="back-to-map-button">
         Back to Map
       </Link>
