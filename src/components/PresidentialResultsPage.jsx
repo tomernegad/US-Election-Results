2075683ab1a0747fbf33d907e304b07b7fd9b47b
Tomer Negad <tomernegad@gmail.com>
2024-12-12 18:03:59 +0200

    improve code performance and change hover color to green.

--- src/components/PresidentialResultsPage.jsx
+++ src/components/PresidentialResultsPage.jsx
@@ -1,23 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import axios from "axios";
+
 import "./PresidentialResultsPage.css";
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
 export default function PresidentialResultsPage() {
   const navigate = useNavigate();
-  const [stateResults, setStateResults] = useState({});
 
   const handleStateClick = (stateName) => {
     navigate(`/state/${stateName}`);
-  };
-
-  const getStateColor = (stateName) => {
-    const result = stateResults[stateName];
-    if (!result) return "#D6D6DA"; // Default color if no result
-    return result.color; // Use the color field from the result
   };
 
   return (
@@ -33,11 +26,11 @@ export default function PresidentialResultsPage() {
                 onClick={() => handleStateClick(geo.properties.name)}
                 style={{
                   default: {
-                    fill: getStateColor(geo.properties.name),
+                    fill: "#D6D6DA",
                     outline: "none",
                   },
-                  hover: { fill: "#F53", outline: "none" },
-                  pressed: { fill: "#E42", outline: "none" },
+                  hover: { fill: "#90EE90", outline: "none" },
+                  pressed: { fill: "#90EE90", outline: "none" },
                 }}
               />
             ))
